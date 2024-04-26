41db5beae438004043ac99776315397328fb7e86
walde <w-schneider1987@web.de>
2024-04-26 15:46:56 +0900

    confu_json version bump

--- conanfile.py
+++ conanfile.py
@@ -15,7 +15,7 @@ class Project(ConanFile):
     def requirements(self):
         self.requires("catch2/2.13.9")
         self.requires("fmt/9.1.0")
-        self.requires("confu_json/1.0.1")
+        self.requires("confu_json/[>=1.0.2 <2]")
         self.requires("boost/1.84.0")
 
 
